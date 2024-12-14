57cd3fc1138abd653bae0be8a8bb6c2858348faf
RemyIsCool <remyrfouquette@gmail.com>
2024-12-13 19:13:44 -0800

    remove harpoon bc i don't use it (sorry prime)

---
+++ nvim/.config/nvim/plugin/lualine.lua
@@ -0,0 +1,16 @@
+if not vim.g.vscode then
+	require("lualine").setup({
+		options = {
+			component_separators = { left = "|", right = "|" },
+			section_separators = { left = "", right = "" },
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_c = { "buffers" },
+			lualine_x = { "encoding", "fileformat", "filetype" },
+			lualine_y = { "progress" },
+			lualine_z = { "location" },
+		},
+	})
+end
